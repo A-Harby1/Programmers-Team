ae7a5631702569fa433fcbb943d90eeddaeb06e4
Ahmed Saleh Harby <45426947+Ahmed-Saleh1@users.noreply.github.com>
2018-12-22 16:31:40 +0200

    Revert "Write posts function"

--- Find_and_Help/src/Interface.java
+++ Find_and_Help/src/Interface.java
@@ -7,23 +7,29 @@ public class Interface {
 		accounts_database ob2=new accounts_database();
 		public Interface()
 		{
-		System.out.println("1-To Sign up");
-		System.out.println("2-To sign in");
-		System.out.println("3-To exit from system");
-		System.out.print("Enter your choice : ");
-		choice_num=input.nextInt();
-		
-		if (choice_num==1)
-		{
-				sign_up();
+				while(true)
+				{
+
+					System.out.println("1-To Sign up");
+					System.out.println("2-To sign in");
+					System.out.println("3-To exit from system");
+					System.out.print("Enter your choice : ");
+					choice_num=input.nextInt();
+					
+					if (choice_num==1)
+					{
+							sign_up();
+					}
+					else if(choice_num==2)
+					{
+					
+					}
+					else if(choice_num==3)
+						break;
+					else 
+						System.out.println("-------Your choice is invalid ,please try again------");
+				}
 		}
-		else if(choice_num==2)
-		{
-		
-		}
-		else 
-			System.out.println("-------Your choice is invalid ,please try again------");
-	}
 		public void sign_up()
 		{
 			System.out.println("------------Sign Up Operation---------------");
@@ -40,11 +46,11 @@ public class Interface {
 				status=ob2.get_account(ob.get_userName());
 			}
 			ob2.save_account(ob.get_name(), ob.get_userName(),ob.get_password(), ob.get_gender(),ob.get_phoneNum());
-			User_Account ua = new User_Account(ob.get_userName());
+		
 		}
 		public void sign_in()
 		{
-			User_Account ua = new User_Account(ob.get_userName());
+			
 		}
 		
 }	
--- Find_and_Help/src/User_Account.java
+++
@@ -1,7 +0,0 @@
-
-public class User_Account {
-	public User_Account(String Username) {
-		Posts ob = new Posts();
-		User_Account_gui uag = new User_Account_gui(ob,Username);
-	}
-}
